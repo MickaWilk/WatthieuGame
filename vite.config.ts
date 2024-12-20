7b07217d0c1a370b4f74b433893dec632bf1f0d3
Michael Wilk <michael.wilk@pichet.com>
2024-12-20 15:40:47 +0100

    fix problems vulnerability

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+import { defineConfig } from 'vite';  
+import react from '@vitejs/plugin-react';  
+import { resolve } from 'path';  
+  
+export default defineConfig({  
+  base: '/WatthieuGame/', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub  
+  plugins: [react()],  
+  resolve: {  
+    alias: {  
+      '@': resolve(__dirname, 'src'),  
+    },  
+  },  
 });
