2f2f5f275aba53886e2c0a28fe1ac648ba82ef52
Wilk Michaël <114574249+MickaWilk@users.noreply.github.com>
2024-12-21 14:18:28 +0100

    update images, spawn bubble

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite';  
-import react from '@vitejs/plugin-react';  
-import { resolve } from 'path';  
-  
-export default defineConfig({  
-  base: '/WatthieuGame/', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub  
-  plugins: [react()],  
-  resolve: {  
-    alias: {  
-      '@': resolve(__dirname, 'src'),  
-    },  
-  },  
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/WatthieuGame/', // Chemin pour GitHub Pages
 });
